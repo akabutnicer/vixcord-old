05a5670447b0f8ba4f4d842151baac5b4b0d7ece
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2023-12-17 08:49:59 -0500

    Update build.py

--- build.py
+++ build.py
@@ -5,5 +5,12 @@ default = pathlib.Path(__file__).resolve().parent
 files_to_minify = [
   default / "index.html",
   default / "server.html",
-  default / "servers/index.html"
+  default / "servers/index.html",
+  default / "sscript.js"
 ]
+
+
+for file in files_to_minify:
+  file_content = file.read_text()
+  minified = minify_html(file_content, minify_js=True, minify_css=True, do_not_minify_doctype=True)
+  file.write_text(minified)
