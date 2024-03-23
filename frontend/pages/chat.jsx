8efa36f3df64f86ad1cf6edfcc3a512af99014f6
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-03-22 21:35:55 -0400

    Update chat.jsx

--- frontend/pages/chat.jsx
+++ frontend/pages/chat.jsx
@@ -201,7 +201,7 @@ export default function HomePage() {
         <div class="flex items-center space-x-2 rtl:space-x-reverse">
             <span class="text-sm font-semibold text-[#727272]">{username}</span>
         </div>
-                      {status.text !== null && <p class="text-sm font-normal py-1 text-[#727272]">{status.text}</p>}
+                      {status.text !== null && <p class="text-sm font-normal py-1 text-[#727272] overflow-hidden text-ellipsis ">{status.text}</p>}
     </div>
 </div>
 
