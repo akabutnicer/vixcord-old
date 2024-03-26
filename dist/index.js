c44960e77d210d3ee6c291dae03aa55829f5eec3
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-03-26 14:21:50 -0400

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
-});
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
-});
-app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
-});
-//# sourceMappingURL=index.js.map
+exports.__esModule = true;
+var user_controller_1 = require("../controllers/user.controller");
+var conversions_controller_1 = require("../controllers/conversions.controller");
+var express_1 = require("express");
+var app = (0, express_1["default"])();
+app.use(express_1["default"].urlencoded({ extended: false }));
+app.use(express_1["default"].json());
+app.get("server/users/create", user_controller_1.createUser);
+app.get("server/users/search", user_controller_1.searchUsers);
+app.post("/conversations/create", conversions_controller_1.createConversation);
+app.get("*/conversations", conversions_controller_1.getAllConversations);
+app.post("/messages/create", conversions_controller_1.addMessageToConversation);
+app.get("/messages/get", conversions_controller_1.getConversationMessages);
+app.listen(4000);
