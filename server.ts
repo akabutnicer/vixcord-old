f3d1cc51d482cf8f8243cc20ff51b1143d7750ec
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-03-26 14:20:47 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,16 +1,35 @@
- import express, { Request, Response } from 'express'
+import * as bodyParser from "body-parser";
+import { createUser, searchUsers } from "./controllers/user.controller";
+import {
+  createConversation,
+  getAllConversations,
+  addMessageToConversation,
+  getConversationMessages,
+} from "./controllers/conversions.controller";
 
-  const app = express()
-  const port = process.env.PORT || 8080
+import express, {Request, Response} from 'express'
 
-  app.get('/', (_req: Request, res: Response) => {
-    return res.send('Express Typescript on Vercel')
-  })
 
-  app.get('/ping', (_req: Request, res: Response) => {
-    return res.send('pong 🏓')
-  })
+import { Server } from "socket.io";
+import http from "http";
+import Socket from "./utils/socket";
+import * as types from "./types";
+import ViteExpress from "vite-express";
 
-  app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`)
-  })
+import * as fs from 'fs';
+import { createServer } from 'vite';
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("server/users/create", createUser);
+app.get("server/users/search", searchUsers);
+app.post("/conversations/create", createConversation);
+app.get("*/conversations", getAllConversations);
+app.post("/messages/create", addMessageToConversation);
+app.get("/messages/get", getConversationMessages);
+app.listen(4000);
+
+
